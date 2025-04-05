22d5eb0aa10a97e630fb39e47c75f12c8e671a32
Nascity <victory8500@naver.com>
2025-04-06 00:50:10 +0900

    Added 'graphical' variable into struct proc.

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  // these are GUI extension of xv6
+  int graphical;
 };
